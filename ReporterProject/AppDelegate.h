4409d01aea7e5a9562581231bfdc44d21fc7c4ef
HuktoDev <hikto583004@list.ru>
2016-04-24 12:20:57 +0500

    Initial Commit

---
+++ ReporterProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ReporterProject
+//
+//  Created by Alexandr Babenko on 24.04.16.
+//  Copyright © 2016 Alexandr Babenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ReporterProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ReporterProject
+//
+//  Created by Alexandr Babenko on 24.04.16.
+//  Copyright © 2016 Alexandr Babenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
